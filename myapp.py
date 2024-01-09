11190c5fa19fefcdcda5c7a142ca124024a374f1
ETONDEV <63426798+ETONDEV@users.noreply.github.com>
2024-01-09 23:19:02 +0400

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -11,7 +11,7 @@ def extract_time(remaining_time):
     remaining_time_formatted = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
     return remaining_time_formatted
 
-def market_status(current_utc, market_tz, market_open, market_close):
+def market_status(current_utc, market_tz, market_open, market_close, xx_time_zone):
     market_time = current_utc.astimezone(market_tz)
 
     if not is_weekday(market_time):
@@ -89,12 +89,12 @@ with col1:
     selected_time_zone = st.selectbox("Select Time Zone :", time_zones)
     clock_container = st.empty()
 with col2:
-    #korea_time_zone = st.text_input("Korea [9:00AM ~ 3:30PM (KST)] :", close_open)
-    korea_time_zone = st.text_input("Korea [9:00AM ~ 3:30PM (KST)] :", korea_market_status, disabled=True)
+    #korea_time_zone = st.text_input("Korea [9:00AM ~ 3:30PM (KST)] :", korea_market_status, disabled=True)
+    korea_time_zone = st.empty()
     stock1_container = st.empty()
 with col3:
-    #us_time_zone = st.text_input("US [11:30PM ~ 6:00AM (KST)] :", close_open)
-    us_time_zone = st.text_input("US [11:30PM ~ 6:00AM (KST)] :", us_market_status, disabled=True)
+    #us_time_zone = st.text_input("US [11:30PM ~ 6:00AM (KST)] :", us_market_status, disabled=True)
+    us_time_zone = st.empty()
     stock2_container = st.empty()
     
 def update_clock():
@@ -109,8 +109,8 @@ def update_clock():
 
     # Display the clock with the time zone label
     clock_container.markdown(f"**Time:** {current_time_formatted}")
-    stock1_container.markdown(market_status(current_time_utc, korean_tz, korean_market_open, korean_market_close))
-    stock2_container.markdown(market_status(current_time_utc, us_tz, us_market_open, us_market_close, us_time_zone))
+    stock1_container.markdown(market_status(current_time_utc, korean_tz, korean_market_open, korean_market_close, korea_time_zone))
+    stock2_container.markdown(market_status(current_time_utc, us_tz, us_market_open, us_market_close, us_time_zone, us_time_zone))
 
 # Call the update_clock function every second
 while True:
