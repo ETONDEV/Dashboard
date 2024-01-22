42e52c4ef9071c6831b80fd06f5d0375d5c7e930
ETONDEV <63426798+ETONDEV@users.noreply.github.com>
2024-01-22 22:15:03 +0900

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -106,16 +106,16 @@ with col4:
     us_time_zone = st.empty()
     stock2_container = st.empty()
 
-tab1, tab2, tab3 = st.tabs(["Main", "Setting1", "Setting2"])
 
+all_coin_list = []
+tab1, tab2, tab3 = st.tabs(["Main", "Setting1", "Setting2"])
 with tab1:
     st.header("Main")
     st.markdown(get_tickers())
-
-with tab2:    
-    sac.transfer(items=[f'item{i}' for i in range(30)], label='label', index=[0, 1], titles=['source', 'target'], reload='reload data', color='dark', search=True, pagination=True, use_container_width=True)#st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+with tab2:
+    all_coin_list = get_tickers()
+    sac.transfer(items=[all_coin_list], label='label', index=[0, 1], titles=['source', 'target'], reload='reload data', color='dark', search=True, pagination=True, use_container_width=True)#st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
    #st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
 with tab3:
     st.header("An owl")
    #st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
