e52373dc6fdd6e8b8dcd55872f84c74609094c5c
ETONDEV <63426798+ETONDEV@users.noreply.github.com>
2024-01-23 19:56:20 +0900

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -138,8 +138,8 @@ with tab2:
     coin_array = coin_selected.write(sac.transfer(items=all_coin_list, label='label', index=[0, 1], titles=['source', 'target'], reload='reload data', color='dark', search=True, pagination=True, use_container_width=True))
     #coin_number = len(coin_array)
 
-    coin_string = ','.join(coin_array)
-    coin_selected2.write(coin_string)
+    #coin_string = ','.join(coin_array)
+    #coin_selected2.write(coin_string)
     
     #coin_selected.write(get_ticker_price(sac.transfer(items=all_coin_list, label='label', index=[0, 1], titles=['source', 'target'], reload='reload data', color='dark', search=True, pagination=True, use_container_width=True)))
     
